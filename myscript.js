97ac7303c9341ee7e8c1672b1afecaf7c35a625a
Emeraldsquid <sharnawildgoose@gmail.com>
2024-06-08 17:09:32 +0100

    08/06/24 x2
    -.js
    -index script - transition
    -home script - image animation

---
+++ myscript.js
@@ -0,0 +1,17 @@
+//index page
+window.addEventListener('load', function() {
+    const enterButton = document.querySelector('.enter');
+    setTimeout(() => {
+        enterButton.style.opacity = 1;
+    }, 2000); // Delay the "ENTER" button appearance by 2 seconds
+});
+
+//homepage images
+window.addEventListener('load', function() {
+    const images = document.querySelectorAll('.welcome-img');
+    images.forEach((img, index) => {
+        setTimeout(() => {
+            img.classList.add('slide-in');
+        }, index * 500); // Delay each image's animation
+    });
+});
